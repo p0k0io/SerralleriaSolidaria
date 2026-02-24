4853763f35f010581cbcd07b3135cf023181e75a
p0k0io <nicolaspenyuelasantiago@gmail.com>
2026-02-24 10:25:35 +0100

    init

---
+++ backend/routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/test', function () {
+    return response()->json([
+        'status' => 'ok',
+        'message' => 'API funcionando correctamente 🚀'
+    ]);
+});
+
+
