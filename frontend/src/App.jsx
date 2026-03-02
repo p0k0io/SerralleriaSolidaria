f41a31c37daf5aaa487abcaffd0913488b43709d
p0k0io <nicolaspenyuelasantiago@gmail.com>
2026-03-02 09:40:46 +0100

    Añadido tailwindCSS en el front end y funcionando

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -28,6 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <h1 className='bg-red-500'>asdasdas</h1>
     </>
   )
 }
