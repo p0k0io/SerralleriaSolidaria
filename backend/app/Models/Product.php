27106bb537185a2ba20d7c9c46d1788b06c36141
eduardmas10 <eduardmaslledo98@gmail.com>
2026-03-09 09:26:34 +0100

    BBDD

---
+++ backend/app/Models/Product.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function variants()
+    {
+        return $this->hasMany(Variant::class);
+    }
+}
